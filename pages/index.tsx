965dec855ceb5e9228a5e641eaa0dd50300f83c3
riz <rizwan.k.off@gmail.com>
2022-10-30 12:15:50 +0530

    initial setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
     <div className="text-4xl space-y-6 font-bold flex flex-col items-center text-gray-800 mt-10 ">
-        <p className="hover:scale-110 transition-all cursor-pointer ">NEXTJS 🖤</p>
-        <p className="hover:scale-110 transition-all cursor-pointer ">TailwindCSS 🌊</p>
-        <p className="hover:scale-110 transition-all cursor-pointer ">Typescript 💓</p>
-        <p className="hover:scale-110 transition-all cursor-pointer ">ESLint 🔷</p>
-        <p className="hover:scale-110 transition-all cursor-pointer ">Prettier 🦋</p>
-        <p className="hover:scale-110 transition-all cursor-pointer ">Husky 🐶</p>
-
-        <h1 className="animate-bounce pt-10">Enjoy!!!!!!!</h1>
+        Test Page
     </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+/* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './layouts/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('daisyui')],
+    daisyui: {
+        styled: true,
+        themes: false,
+        base: false,
+        utils: true,
+        logs: false,
+        rtl: false,
+        prefix: '',
+        darkTheme: 'dark',
+    },
 };
